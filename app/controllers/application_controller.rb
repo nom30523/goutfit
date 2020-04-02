8c3b4ad36e60083ce6d281b45a8d589cbcd5a569
nom30523 <60598776+nom30523@users.noreply.github.com>
2020-04-03 01:03:27 +0900

    error-handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  before_action :move_to_root_path, except: [:create, :destroy, :update], if: :devise_controller?
+
+  protected
+
+  def move_to_root_path
+    redirect_to root_path
+  end
 end
