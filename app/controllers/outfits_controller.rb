47661dcb3e3487842f465c867cc3dc94ac409a2e
nom30523 <60598776+nom30523@users.noreply.github.com>
2020-04-05 19:52:01 +0900

    edit-outfit-controller

--- app/controllers/outfits_controller.rb
+++ app/controllers/outfits_controller.rb
@@ -3,6 +3,13 @@ class OutfitsController < ApplicationController
   before_action :move_to_top
 
   def index
+    @outfit = Outfit.new
+  end
+
+  def create
+    @outfit = Outfit.new(outfit_params)
+    @outfit.create
+    redirect_to action: :index
   end
 
   private
@@ -10,4 +17,9 @@ class OutfitsController < ApplicationController
   def move_to_top
     redirect_to root_path unless user_signed_in?
   end
+
+  def outfit_params
+    params.require(:outfit).permit(:image).merge(user_id: current_user.id)
+  end
+
 end
