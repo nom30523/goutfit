125a9cad7253c8a8b628b2fece5b7079638548e0
nom30523 <60598776+nom30523@users.noreply.github.com>
2020-04-14 00:55:49 +0900

    maigration-fix

--- db/migrate/20200407083158_create_posts.rb
+++ db/migrate/20200407083158_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.references :outfit, foreign_key: true
+      t.references :outfit, foreign_key: true
       t.references :user, foreign_key: true, index: false
       t.date :appointed_day, null: false
       
