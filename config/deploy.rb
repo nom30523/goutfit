e97189ce21f1403ca3e96394674e87c85f2c7ac0
nom30523 <60598776+nom30523@users.noreply.github.com>
2020-04-14 12:21:20 +0900

    capistrano-setting

---
+++ config/deploy.rb
@@ -0,0 +1,24 @@
+lock '3.13.0'
+
+set :application, 'goutfit'
+
+set :repo_url, 'git@github.com:nom30523/goutfit.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.5.1'
+
+set :ssh_options, auth_methods: ['publickey'], keys: ['~/.ssh/goutfit_key_rsa']
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
