bf9d989d42f2ed950a42d65702bce2fa04b15901
nom30523 <60598776+nom30523@users.noreply.github.com>
2020-04-14 14:26:07 +0900

    capistrano-setting-change3

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,6 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
-set :ssh_options, auth_methods: ['publickey'], keys: ['~/.ssh/goutfit.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
